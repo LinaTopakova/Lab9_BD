61e355e2f0bda43ef385c934c062bfd85b76c51d
Udynix <129510560+LinaTopakova@users.noreply.github.com>
2025-06-17 20:50:14 +0700

    1234

--- models/article.js
+++ models/article.js
@@ -2,17 +2,17 @@ const mongoose = require('mongoose');
 
 const reviewSchema = new mongoose.Schema({
     name: { type: String, required: true },
-    message: { type: String, required: true },
-    rating: { type: Number, required: true, min: 1, max: 10 }
-});
+    text: { type: String, required: true },
+    rating: { type: Number, required: true, min: 1, max: 10,required: true }
+},{_id: false});
 
 const articleSchema = new mongoose.Schema({
     title: { type: String, required: true },
-    authors: { type: [String], required: true },
-    publishDate: { type: Date, default: Date.now },
+    authors: [{ type: String, required: true }],
+    postedAt: { type: Date, default: Date.now },
     content: { type: String, required: true },
-    tags: { type: [String] },
+    tags: [{ type: String}],
     reviews: [reviewSchema]
 });
 
-module.exports = mongoose.model('Article', articleSchema);
+module.exports = mongoose.model('article', articleSchema);
